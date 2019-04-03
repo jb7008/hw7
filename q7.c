9c6b23059ddc6528cf7f506b478c680ee0237f40
Josh Booth <jb7008@ship.edu>
2019-04-03 09:25:51 -0400

    d

--- q7.c
+++ q7.c
@@ -6,35 +6,67 @@
  */
 
 #include <stdio.h> /* Includes printf, scanf */
+#include <string.h> /* Includes strncpy and strcpy */
 
-#define MAX_STR_LEN 80
+#define MAX_STR_LEN 80 /* Max length of a string */
+#define MAX_NUM_OF_WORDS 16 /* Number of words that can be inputted */
 
-void get_line(char line[MAX_STR_LEN]);
-void split_line(char line[MAX_STR_LEN], char word1[MAX_STR_LEN], char word2[MAX_STR_LEN]);
+void get_line(char line[MAX_STR_LEN]); /* Reads input line */
+  /* Splits and line on spaces and prints reversed words */
+void reverse_line(char line[MAX_STR_LEN],\
+  char words[MAX_NUM_OF_WORDS][MAX_STR_LEN]);
+
 int
 main(void)
 {
-  char input_line[MAX_STR_LEN],
-    word1[MAX_STR_LEN],
-    word2[MAX_STR_LEN];
+  char input_line[MAX_STR_LEN], /* Holds input string */
+    words[MAX_NUM_OF_WORDS][MAX_STR_LEN]; /* Holds individual words */
 
-  get_line(input_line);
+  get_line(input_line); /* Gets input line */
+  /* Splits and line on spaces and prints reversed words */
+  reverse_line(input_line, words);
+
+
 
   return 0;
 }
 
+/* Reads input line */
 void
 get_line(char line[MAX_STR_LEN])
 {
   printf("Input: ");
-  scanf("%s", line);
+  gets(line);
 }
 
+/* Splits and line on spaces and prints reversed words */
 void
-split_line(char line[MAX_STR_LEN], char word1[MAX_STR_LEN], char word2[MAX_STR_LEN])
+reverse_line(char line[MAX_STR_LEN], char words[MAX_NUM_OF_WORDS][MAX_STR_LEN])
 {
-  int and_index;
-  char combine_word[] = "and";
+    int space_index = 0, /* Current word in words[][] */
+    i = 0; /* Iterator through line */
 
-  and_index = find_match_index(line, char match_word);
+  while(i < strlen(line)) /* While end hasn't been reached */
+  {
+    if(line[i++] == ' ') /* If there is a space */
+    {
+      /* Copy string up to that point into words
+        ( - 1 removes space from copy) */
+      strncpy(words[space_index++], line, i - 1);
+      line = line + i; /* Moves line onto next chracter after space */
+      i = 0; /* Reset i */
+    }
+  }
+  /* Copy the last word, since no space will be found */
+  strcpy(words[space_index], line);
+  /* Formatting */
+  printf("Output: ");
+  /* Print the words in words[][] in reverse order */
+  printf("%s\n", words[0]);
+  printf("%s\n", words[1]);
+  printf("%s\n", words[2]);
+  while(space_index >= 0)
+    printf("%s ", words[space_index--]);
+  printf("\n");
 }
+
--- q8.c
+++ q8.c
@@ -6,37 +6,52 @@
  */
 
 #include <stdio.h> /* Includes printf, scanf */
+#include <string.h> /* Includes strlen */
 
-#define MAX_WORD_LEN 80
+#define MAX_WORD_LEN 80 /* Max length of a string */
 
+/* Gets index of the first word at the first difference between the two words */
 int cmp_ends(char word1[MAX_WORD_LEN], char word2[MAX_WORD_LEN]);
 
 int
 main(void)
 {
-  int index_from_end;
-  char word1[MAX_WORD_LEN],
-    word2[MAX_WORD_LEN];
+  int index_of_diff; /* Index of difference in first word */
+  char word1[MAX_WORD_LEN], /* 1st word */
+    word2[MAX_WORD_LEN]; /* 2nd word */
 
+  /* Gets words */
   printf("Enter word1: ");
   scanf("%s", word1);
 
   printf("Enter word2: ");
   scanf("%s", word2);
 
-  index_from_end =  cmp_ends(word1, word2);
+  /* Gets index of the first word at the first difference
+    between the two words */
+  index_of_diff =  cmp_ends(word1, word2);
+
+  /* Prints the last portion of word1 (part that is the same between the words)
+    the additional operations add the offset of the difference index to word1,
+    with the +1 being used to convert from index to memory amount */
+  printf("%s\n", word1 + strlen(word1) - index_of_diff + 1);
+
   return 0;
 }
 
+/* Gets index of the first word at the first difference between the two words */
 int
 cmp_ends(char word1[MAX_WORD_LEN], char word2[MAX_WORD_LEN])
 {
-  int word1_len = 0,
-    word2_len = 0;
-  while(word1[word1_len++]);
-  word1_len--;
-  while(word2[word2_len++]);
-  word2_len--;
-  while(word1[
+  int word1_len = strlen(word1), /* Length of first word */
+    word2_len = strlen(word2); /* Length of second word */
 
+  /* Reduce the word lengths until a difference in found */
+  while(word1[word1_len] == word2[word2_len])
+  {
+    word1_len--;
+    word2_len--;
+  }
+
+  return(strlen(word1) - word1_len); /* Returns index of first differing char */
 }
